28ee1d4940d74f1ac7d77fd62b37221059fd4647
backupARuan <backup20250313@gmail.com>
2025-12-15 15:55:53 +0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,6 @@
+const products = [
+    { name: "禾家香手工水餃 100 粒", price: "$180", date: "12/30", link: "https://www.facebook.com/groups/671943685418673/permalink/849037494375957/" },
+    { name: "蝦界的愛馬仕-泰蝦仁量販包（大）", price: "$240", date: "12/30", link: "https://www.facebook.com/groups/671943685418673/permalink/849035704376136/" },
+    { name: "火腿QQ丁 x 1000g", price: "$110", date: "12/15", link: "https://www.facebook.com/groups/671943685418673/permalink/849034337709606/" },
+    { name: "德國豬腳", price: "$175", date: "12/16", link: "https://www.facebook.com/groups/671943685418673/permalink/849033071043066/" }
+];
